f7f1e592fc5ea9efd235fa27ecc088a61c1f6db0
Jorge Ronseria <jaronseriam@correo.udistrital.edu.co>
2020-11-14 22:16:25 -0500

    lectura de los datos del archivo .json

---
+++ server/storage/index.js
@@ -0,0 +1,15 @@
+const fs = require('fs'),
+      path = require('path');
+
+module.exports = {
+    getDataAll: () => {
+            var self = this;
+            let dataPath = __dirname + path.join('/data/data.json');
+            return new Promise((resolve, reject) => {
+                fs.readFile(dataPath, 'utf8', (err, readData) => {
+                    if (err) reject(err)
+                    resolve(JSON.parse(readData));
+                });
+            }); 
+        } 
+}; 
